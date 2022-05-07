b2d6695e13ec479880ad804215bf11d4aa1c2b18
Gcarlosviza <56936797+Gcarlosviza@users.noreply.github.com>
2022-05-07 11:13:44 -0400

    Add files via upload

---
+++ Sys-Dev-Project-George/app/core/DBConnection.php
@@ -0,0 +1,23 @@
+<?php 
+namespace app\core;
+
+class DBConnection {
+
+	static $connection = null;
+
+	private function __construct() {
+		if (self::$connection == null) {
+			$host = 'localhost';
+			$DBname = 'redteam';
+			$user = 'root';
+			$password = '';
+			self::$connection = new \PDO("mysql:host=$host;dbname=$DBname", $user, $password);
+		}
+	}
+
+	public static function getInstance() {
+		new DBConnection();
+		return self::$connection;
+	}
+
+}
